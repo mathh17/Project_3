57446aebea074c0d46750b52d1277c36836750f6
Joachim Skovbogaard <Joachim.skovbogaard@gmail.com>
2021-04-27 13:12:44 +0200

    Virker ikke, men saa er den der

--- Encode.py
+++ Encode.py
@@ -6,34 +6,65 @@
 #%%
 import sys
 import bitIO as bit
-file = open("C:/Users/oeste/OneDrive/Uni/data_2_semester/Algoritmer/Project_3/DelIIITestFilerTilUdlevering/KingJamesBible.txt", 'rb')
+import PQHeap 
+from Element import Element
+
+#file = open("C:/Users/oeste/OneDrive/Uni/data_2_semester/Algoritmer/Project_3/
+file = open("DelIIITestFilerTilUdlevering/readMe.txt", 'rb')
+
 b = file.read()
-print(len(b))
+#print(len(b))
 bit_class = bit.BitWriter(file)
 freqTabel = [0]*255
 for i in b:
     freqTabel[i] +=1
-print(freqTabel)
 
-file.close()
-# %%
+pq = []
+for i in range(len(freqTabel)):
+    e = Element(freqTabel[i], [i])
+    PQHeap.insert(pq, e)
+    
+while len(pq) > 0:
+    # Udtag det Element fra prioritetskøen, som har mindste key.
+    e = PQHeap.extractMin(pq)
+    # Tilgå og print dets felter key og data.
+    extractedKey = e.key
+    extractedData = e.data
+    print(extractedKey)
+    print(extractedData)
 
-import PQHeap as pq
+
+
 
 def huffmann(C):
     n = len(C)
     Q = C
-    for i in range(1, n-1):
-        x = pq.extractMin(Q)
-        y = pq.extractMin(Q)
-        z = x + y
-        pq.insert(Q,z)
-        return pq.extractMin(Q)
-    
+    for i in range(0, n-1):
+        x = PQHeap.extractMin(Q).key
+        y = PQHeap.extractMin(Q).key
+        z = Element(x+y,[[x],[y]])
+        PQHeap.insert(Q,z)
+    return PQHeap.extractMin(Q)
 
+
+
+huffmann(pq)
+
+
+file.close()
 # %%
 
-import Element as el
+from Element import Element
+import PQHeap
 
 
-el1 = el(1000)
+el1 = Element(1000, [97])
+
+pq = []
+PQHeap.insert(pq,el1)
+
+e = PQHeap.extractMin(pq)
+extractedKey = e.key
+extractedData = e.data
+print(extractedKey)
+print(extractedData)
