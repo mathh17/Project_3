7bdd2b1f05a327250bea3e0c29b2bd9e897beb4a
Mathias <mathh17@student.sdu.dk>
2021-04-27 11:44:12 +0200

    file reader

--- Encode.py
+++ Encode.py
@@ -3,5 +3,13 @@
          Joachim Skovbogaard - Josko20
          Andreas Klauber - ankla20
 """
-
+#%%
 import sys
+import bitIO as bit
+file = open("C:/Users/oeste/OneDrive/Uni/data_2_semester/Algoritmer/Project_3/DelIIITestFilerTilUdlevering/oneByte.txt", 'rb')
+b = file.read()
+print(len(b))
+bit_class = bit.BitReader(file)
+bit_class.read(b)
+file.close()
+# %%
